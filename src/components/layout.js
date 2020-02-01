45a495f40843cdb2f8242181d508e5529859aa5e
Cspeisman <corey@blueorange.digital>
2020-02-01 16:38:24 -0500

    Adds custom fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,6 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
@@ -17,6 +16,7 @@ const Layout = ({ children }) => {
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
+          textAlign: 'center'
         }}
       >
         <main>{children}</main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const upperCase = {textTransform: 'uppercase'}
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi 👋</h1>
-    <div>Welcome to Corey and Alisha's wedding website!</div>
+    <h2>Alisha & Corey</h2>
+    <div className="welcome">
+      <div style={upperCase}>are getting married</div>
+        <div>in</div>
+        <div style={upperCase}>Washington, D.C.</div>
+        <div>on</div>
+    </div>
+    <h2>Sunday, October 11, 2020</h2>
   </Layout>
 )
 
