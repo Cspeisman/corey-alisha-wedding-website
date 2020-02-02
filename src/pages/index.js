42c92628df9b4730c6e27112a854b66f09e0bf5c
Cspeisman <corey@blueorange.digital>
2020-02-02 13:22:46 -0500

    show me the bowies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { BowieImages } from "../components/BowieImages"
 
 const upperCase = { textTransform: "uppercase" }
 const lineHotelFigmaURL = "https://s3-alpha-sig.figma.com/img/3216/1c63/3425bd2c7eb323409cd32240a2360adc?Expires=1581292800&Signature=NIum12quYCu7vu1uhAriPcKiVIMd3lpFi6xJaCT2OkFwjGtJEPSYMcCNejfFGBj3pOZRsBvNB-xpFwALX2VFbY8eahx5VB~M3mjgYTp6tcNPrpUFTQOXqVvbqzJFdzpCz692Y~5Nz9e3Cek-uVx82vfnH2ChUvFH4SwHFWtutNKbJWxf-2HS11blUzS9at8fTK9-I5HDUFggblSkf6NAXjzpNoT3ez7f~QEnWbTIfAoGAOO5d1iGKChC25VdroMllp~j4Gc-iRWI7dS4H2QVnL3jNv-IabOrY-HJc4T0-9Spa3W1kOg4YDckI5rXK2TAN2tEzaNOcyn7EESiF1EpNg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home"/>
-    <h2>Alisha & Corey</h2>
-    <div className="section">
-      <div style={upperCase}>are getting married</div>
-      <div>in</div>
-      <div style={upperCase}>Washington, D.C.</div>
-      <div>on</div>
-    </div>
+const IndexPage = () => {
+  const [showBowie, setShowBowie] = useState(false)
 
-    <div className="section">
-      <h2>Sunday, October 11, 2020</h2>
-      <div>at</div>
-      <div>
-        <a href="https://www.thelinehotel.com/dc">THE LINE HOTEL D.C.</a>
+  return (
+    <Layout>
+      <div style={{
+        display: "flex",
+        flexFlow: "row-reverse",
+        width: "100%",
+      }}>
+        <div onClick={() => setShowBowie(true)}>♡︎</div>
       </div>
-      <img src={lineHotelFigmaURL} alt="the-line-hotel" width={600}/>
-    </div>
+      <SEO title="Home"/>
+      <h2>Alisha & Corey</h2>
+      <div className="section">
+        <div style={upperCase}>are getting married</div>
+        <div>in</div>
+        <div style={upperCase}>Washington, D.C.</div>
+        <div>on</div>
+      </div>
 
-    <div className="section">
-      <div>SCHEDULE, TRAVEL, REGISTRY, AND RSVP DETAILS</div>
-      <div>Coming soon</div>
-    </div>
+      <div className="section">
+        <h2>Sunday, October 11, 2020</h2>
+        <div>at</div>
+        <div>
+          <a href="https://www.thelinehotel.com/dc">THE LINE HOTEL D.C.</a>
+        </div>
+        <img src={lineHotelFigmaURL} alt="the-line-hotel" width={600}/>
+      </div>
 
-    <div className="section">
-      <div>👰🏻🤵🏼</div>
-      <h3>#AlishasMainSpeis</h3>
-    </div>
-  </Layout>
-)
+      <div className="section">
+        <div>SCHEDULE, TRAVEL, REGISTRY, AND RSVP DETAILS</div>
+        <div>Coming soon</div>
+      </div>
+
+      <div className="section">
+        <div>👰🏻🤵🏼</div>
+        <h3>#AlishasMainSpeis</h3>
+      </div>
+      {showBowie && <BowieImages setShowBowie={setShowBowie}/>}
+    </Layout>
+  )
+}
 
 export default IndexPage
