88e2ddee1dc31e5ab2c1cd6ac952092442fe0c18
Cspeisman <corey@blueorange.digital>
2020-02-14 15:01:05 -0500

    serve our own images

--- src/components/BowieImages.js
+++ src/components/BowieImages.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react"
+import BowieURL from "../images/bowie.png"
 
-
-const bowieUrl = "https://s3-alpha-sig.figma.com/img/35b5/903a/be419051118abc21bb5c95df105fc422?Expires=1581292800&Signature=dvxHHtN72w0bYmw50zTImGz5qiYXVY9-RtubZf9G8zqFLzPkMqoeBTNLI-oRPqXYJ3pz0DpnPlCgfEKT0EpnUo0BMg5LcmboDtWhoDdc~DHjPP8X3CaBJo0yBUXX3e0Ecqfj87or31NOPq0sMIbsNB5E1~2UDYx9TG4MhbK2NuaVFNUQOqWCp5kDSf321YeWdUP76eLOc02upnLT39phazJP52CV1gRWbrGvNsiNsMyyFX5ss-80WB9eGxHq~yvBYgJF8J26lX-YVCLC-PaVmzSR92EBF1Nu5egoF1dvDeE8kh9y5rDSEWTj0agN0QELjPdT79saj-Co6tjL3LOY3Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
 
 const Bowie = () => (
   <div style={{
@@ -9,15 +8,13 @@ const Bowie = () => (
     top: Math.floor(Math.random() * Math.floor(window.innerHeight)),
     left: Math.floor(Math.random() * Math.floor(window.innerWidth)),
   }}>
-    <svg width="301" height="324" viewBox="0 0 301 324" fill="none" xmlns="http://www.w3.org/2000/svg"
-         xmlnsXlink="http://www.w3.org/1999/xlink">
+    <svg width="301" height="324" viewBox="0 0 301 324" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="-28" y="-5" width="329" height="329" fill="url(#pattern0)"/>
       <defs>
         <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
           <use xlinkHref="#image0" transform="scale(0.002)"/>
         </pattern>
-        <image id="image0" width="500" height="500"
-               href={bowieUrl}/>
+        <image id="image0" href={BowieURL}/>
       </defs>
     </svg>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import React, { useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { BowieImages } from "../components/BowieImages"
+import TheLineHotel from "../images/the-line.png"
 
 const upperCase = { textTransform: "uppercase" }
-const lineHotelFigmaURL = "https://s3-alpha-sig.figma.com/img/3216/1c63/3425bd2c7eb323409cd32240a2360adc?Expires=1581292800&Signature=NIum12quYCu7vu1uhAriPcKiVIMd3lpFi6xJaCT2OkFwjGtJEPSYMcCNejfFGBj3pOZRsBvNB-xpFwALX2VFbY8eahx5VB~M3mjgYTp6tcNPrpUFTQOXqVvbqzJFdzpCz692Y~5Nz9e3Cek-uVx82vfnH2ChUvFH4SwHFWtutNKbJWxf-2HS11blUzS9at8fTK9-I5HDUFggblSkf6NAXjzpNoT3ez7f~QEnWbTIfAoGAOO5d1iGKChC25VdroMllp~j4Gc-iRWI7dS4H2QVnL3jNv-IabOrY-HJc4T0-9Spa3W1kOg4YDckI5rXK2TAN2tEzaNOcyn7EESiF1EpNg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
 
 const IndexPage = () => {
   const [showBowie, setShowBowie] = useState(false)
@@ -17,7 +17,7 @@ const IndexPage = () => {
         flexFlow: "row-reverse",
         width: "100%",
       }}>
-        <div onClick={() => setShowBowie(true)}>♡︎</div>
+        <span style={{cursor: "pointer"}} role="img" onClick={() => setShowBowie(true)}>♡︎</span>
       </div>
       <SEO title="Home"/>
       <h2>Alisha & Corey</h2>
@@ -34,7 +34,7 @@ const IndexPage = () => {
         <div>
           <a href="https://www.thelinehotel.com/dc">THE LINE HOTEL D.C.</a>
         </div>
-        <img src={lineHotelFigmaURL} alt="the-line-hotel" width={600}/>
+        <img src={TheLineHotel} alt="the-line-hotel" width={600}/>
       </div>
 
       <div className="section">
