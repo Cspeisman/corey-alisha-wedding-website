ecd05ec8c5643eebae1fafd51e90345bc4aa5f23
cspeisman <cspeisman@gmail.com>
2021-09-24 21:05:02 -0400

    update with new party info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import TheLineHotel from "../images/the-line.png"
+import TheRiggsHotel from "../images/riggs-hotel.jpeg"
 import '../components/typography.css';
 import '../components/global.css';
 const upperCase = {textTransform: "uppercase"}
@@ -12,41 +12,37 @@ const IndexPage = () => {
           <h2 style={{marginTop: '10rem'}}>Alisha & Corey</h2>
         </div>
         <div className="section">
-          <div style={upperCase}>are getting married</div>
+          <div style={upperCase}>are celebrating their wedding</div>
           <div>in</div>
           <div style={upperCase}>Washington, D.C.</div>
           <div>on</div>
         </div>
 
         <div className="section">
-          <h2>Sunday, October 11, 2020</h2>
+          <h2>Friday, November 5th, 2021</h2>
         </div>
 
         <div className="section">
           <div>at</div>
           <div>
-            <a target="_blank" href="https://www.thelinehotel.com/dc">THE LINE HOTEL D.C.</a>
+            <a target="_blank" href="https://www.riggsdc.com/">THE RIGGS HOTEL</a>
           </div>
-          <div>(now in micro-ceremony - 2021 party plans to come)</div>
         </div>
 
         <div className="section">
-          <img src={TheLineHotel} alt="the-line-hotel" style={{maxWidth: '100%'}}/>
+          <img src={TheRiggsHotel} alt="the-riggs-hotel" style={{maxWidth: '100%'}}/>
         </div>
         <div className="section">
           <div>REGISTRY</div>
           <div>
             <div>
               <a target="_blank" href="https://www.zola.com/registry/coreyandalisha">Zola</a>
-
             </div>
             <div>
-
               <a target="_blank" href="https://www.crateandbarrel.com/gift-registry/alisha-ramos-and-corey-speisman/r6159047">Crate &amp; Barrel</a>
             </div>
           </div>
         </div>
-
         <div className="section">
           <div>👰🏻🤵🏼</div>
           <h3>#AlishasMainSpeis</h3>
